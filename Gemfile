526bc9f5d3c39bf5e830e49aabdb2c6233e3f30d
Matthieu Grebot <matcoopa@gmail.com>
2016-11-24 17:56:53 +0000

    Basic htm/css skeleton with Bourbon suite and Scrollmagic

---
+++ Gemfile
@@ -0,0 +1,34 @@
+# If you do not have OpenSSL installed, change
+# the following line to use 'http://'
+source 'https://rubygems.org'
+
+# For faster file watcher updates on Windows:
+gem 'wdm', '~> 0.1.0', platforms: [:mswin, :mingw]
+
+# Windows does not come with time zone data
+gem 'tzinfo-data', platforms: [:mswin, :mingw, :jruby]
+
+# Middleman Gems
+gem 'middleman', '>= 4.0.0'
+gem 'middleman-livereload'
+
+# Deploy
+gem 'middleman-deploy', '~> 2.0.0.pre.alpha'
+
+# Bourbon setup
+gem 'bourbon'
+gem 'neat'
+gem 'bitters'
+
+gem 'middleman-sprockets', '4.0.0.rc.3'
+
+source 'https://rails-assets.org' do
+  gem 'rails-assets-jquery', '2.1.1'
+# useful carousel  
+  gem 'rails-assets-slick.js', '1.5.7'
+end
+
+
+
+# JQuery
+gem "jquery-middleman"
---
+++ config.rb
@@ -0,0 +1,62 @@
+###
+# Page options, layouts, aliases and proxies
+###
+
+# Per-page layout changes:
+#
+# With no layout
+page '/*.xml', layout: false
+page '/*.json', layout: false
+page '/*.txt', layout: false
+
+# With alternative layout
+# page "/path/to/file.html", layout: :otherlayout
+
+# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
+# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
+#  which_fake_page: "Rendering a fake page with a local variable" }
+
+# General configuration
+
+activate :sprockets
+
+if defined? RailsAssets
+  RailsAssets.load_paths.each do |path|
+    sprockets.append_path path
+  end
+end
+
+
+# Reload the browser automatically whenever files change
+configure :development do
+  activate :livereload
+end
+
+###
+# Helpers
+###
+
+# Methods defined in the helpers block are available in templates
+# helpers do
+#   def some_helper
+#     "Helping"
+#   end
+# end
+
+# Build-specific configuration
+configure :build do
+  # smarter assets
+  activate :asset_hash
+  activate :minify_javascript
+  activate :minify_css
+  activate :gzip
+  activate :relative_assets
+end
+
+activate :deploy do |deploy|
+  deploy.deploy_method = :git
+  deploy.branch = 'gh-pages'
+  deploy.build_before = true
+end
+
+set :relative_links, true

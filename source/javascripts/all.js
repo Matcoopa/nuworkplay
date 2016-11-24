1a068193ec3409859ace72ddeec2b6b4253c3bc4
Matthieu Grebot <matcoopa@gmail.com>
2016-11-24 20:07:53 +0000

    Adding controller and 2 Scenes for Pinned bcg

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,28 @@
 //= require jquery
+$(document).ready(function(){
+
+// Init ScrollMagic
+var controller = new ScrollMagic.Controller();
+
+// pin bcg1
+var pinBcg1 = new ScrollMagic.Scene({
+  triggerElement: '.bcg-parallax',
+  triggerHook: 0,
+  duration: '50%'
+})
+  .setPin('.bcg1')
+  .addIndicators()
+  .addTo(controller);
+
+
+
+var pinBcg2 = new ScrollMagic.Scene({
+  triggerElement: '.bcg2',
+  triggerHook: 0,
+  duration: 300
+})
+  .setPin('bcg2')
+  .addIndicators()
+  .addTo(controller);
+
+});

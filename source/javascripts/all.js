314dcfcd0f525ba689996914d2b98249e6b71950
Matthieu Grebot <matcoopa@gmail.com>
2016-11-25 23:27:12 +0000

    Better scrolling xp, need to fix wrapper and form wrapper

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -5,44 +5,40 @@
 	// Init ScrollMagic
     var controller = new ScrollMagic.Controller();
 
-    // get all slides
-	var slides = ["#slide01"];
-
-	// get all break up sections
-	// var breakSections = ["#cb01"];
-
 
 	    // SCENE 4 - parallax effect on each of the slides with bcg
 	    // move bcg container when slide gets into the view
-		slides.forEach(function (slide, index) {
 
-			var $bcg = $(slide).find('.bcg');
+
+			var $bcg = $('#slide01').find('.bcg');
 
 			var slideParallaxScene = new ScrollMagic.Scene({
-		        triggerElement: slide,
+		        triggerElement: '#slide01',
 		        triggerHook: 1,
 		        duration: "100%"
 		    })
-		    .setTween(TweenMax.from($bcg, 1, {y: '-40%', autoAlpha: 0.3, ease:Power0.easeNone}))
+		    .setTween(TweenMax.from($bcg, 1, {y: '-30%', autoAlpha: 0, ease:Power0.easeNone}))
+        .addIndicators()
 		    .addTo(controller);
-	    });
+
 
 	    // SCENE 5 - parallax effect on the intro slide
 	    // move bcg container when intro gets out of the the view
 
 	    var introTl = new TimelineMax();
-
+    //
 	    introTl
-	    	.to($('#intro header, .scroll-hint'), 0.2, {autoAlpha: 0, ease:Power1.easeNone})
+	    	.to($('#intro header'), 0.2, {autoAlpha: 0, ease:Power1.easeNone})
 	    	.to($('#intro .bcg'), 1.4, {y: '20%', ease:Power1.easeOut}, '-=0.2')
-	    	.to($('#intro'), 0.7, {autoAlpha: 0.5, ease:Power1.easeNone}, '-=1.4');
+	    	.to($('#intro .bcg'), 0.2, {autoAlpha: 0.8, ease:Power1.easeNone}, '-=1.4');
 
 		var introScene = new ScrollMagic.Scene({
 	        triggerElement: '#intro',
 	        triggerHook: 0,
-	        duration: "100%"
+	        duration: "150%"
 	    })
 	    .setTween(introTl)
+      .addIndicators()
 	    .addTo(controller);
 
 });

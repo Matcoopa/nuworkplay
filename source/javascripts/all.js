1898ef8a8fc40ff5469f2a0da009c576df793fdb
Matthieu Grebot <matcoopa@gmail.com>
2016-11-26 19:17:03 +0000

    Working scrolling and end wrapper

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -15,12 +15,15 @@
  		    .setTween(TweenMax.from($bcg, 1, {y: '-40%', autoAlpha: 0.3, ease:Power0.easeNone}))
  		    .addTo(controller);
 
+
+    // Create Tween Scene for Intro
+
  	    var introTl = new TimelineMax();
 
  	    introTl
- 	    	.to($('#intro header, .scroll-hint'), 0.2, {autoAlpha: 0, ease:Power1.easeNone})
+ 	    	.to($('#intro header'), 0.2, {autoAlpha: 0, ease:Power1.easeNone})
  	    	.to($('#intro .bcg'), 1.4, {y: '20%', ease:Power1.easeOut}, '-=0.2')
- 	    	.to($('#intro'), 0.7, {autoAlpha: 0.5, ease:Power1.easeNone}, '-=1.4');
+ 	    	.to($('#intro'), 0.7, {autoAlpha: 0.7, ease:Power1.easeNone}, '-=1.4');
 
  		var introScene = new ScrollMagic.Scene({
  	        triggerElement: '#intro',
@@ -29,6 +32,27 @@
  	    })
  	    .setTween(introTl)
  	    .addTo(controller);
+
+    // Create Pin Scene for Intro background-color
+
+      var pinIntro = new ScrollMagic.Scene({
+          triggerElement: '#cb01',
+          triggerHook: 0.5,
+          duration: '30%'
+      })
+      .setPin('#intro .bcg')
+      .addTo(controller);
+
+    // Create Pin Scene for slide1 at the end
+
+      var pinEnd = new ScrollMagic.Scene({
+        triggerElement: '#slide01',
+        triggerHook: 0,
+      })
+      .setPin('#slide01')
+      .addTo(controller);
+
+
 
  	    // change behaviour of controller to animate scroll instead of jump
  		controller.scrollTo(function (newpos) {

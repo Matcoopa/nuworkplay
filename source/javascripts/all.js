08150ea8c56bef4782ffd7ba87f3f41c3cb53f41
Matthieu Grebot <matcoopa@gmail.com>
2016-11-27 00:30:50 +0000

    Working modal as CTA on intro

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -60,14 +60,30 @@
  		});
 
 
+    // Modal activation from Bourbon/Refills
 
-<!-- for button click to reach contact form -->
+    $(function() {
+      $("#modal-1").on("change", function() {
+        if ($(this).is(":checked")) {
+          $("body").addClass("modal-open");
+        } else {
+          $("body").removeClass("modal-open");
+        }
+      });
 
-  $("button").click(function() {
-    $('html,body').animate({
-      scrollTop: $(".slidInUp").offset().top},
-        'slow');
-  })
+      $(".modal-fade-screen, .modal-close").on("click", function() {
+        $(".modal-state:checked").prop("checked", false).change();
+      });
+
+      $(".modal-inner").on("click", function(e) {
+        e.stopPropagation();
+      });
+    });
+
+
+
+
+
 
 
 });
